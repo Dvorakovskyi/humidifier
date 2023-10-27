216842083f972e5f088c7ea8106f539296a56b23
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-10-27 11:17:04 +0300

    update code

---
+++ src/api/CRM.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://allgoodsbazaar.lp-crm.biz/api/addNewOrder.html';
+const API_KEY = 'badb711cc1aeed31c0a43d20d9697d28';
+const country = 'UA';
+const products = ['Термос'];
+
+export const newOrder = (name, phone, color) => {
+    
+}
