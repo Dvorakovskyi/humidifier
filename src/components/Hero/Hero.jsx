8e9464412b2e226638f92cccecb493d5f1ed66a4
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-10-28 20:39:02 +0300

    update Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import mainPhoto from '../../images/hero/mainPhoto.png';
-import benefitsImg from '../../images/hero/benefits.jpg';
-import { StyledMainTitle, StyledHeroImg } from './Hero.styled';
+import icon1 from '../../images/hero/icon1.png';
+import icon2 from '../../images/hero/icon2.png';
+import icon3 from '../../images/hero/icon3.png';
+import {
+  StyledMainTitle,
+  StyledHeroImg,
+  StyledHeroItem,
+  StyledHeroList,
+  StyledItemText,
+} from './Hero.styled';
 
 const Hero = () => {
   return (
@@ -10,7 +18,20 @@ const Hero = () => {
         Термос TermoComfort 500мл із датчиком температури
       </StyledMainTitle>
       <StyledHeroImg src={mainPhoto} alt="Термос" />
-      <StyledHeroImg src={benefitsImg} alt="Переваги" />
+      <StyledHeroList>
+        <StyledHeroItem>
+          <img src={icon1} alt="icon" width="30" height="30" />
+          <StyledItemText>Зручність та якість</StyledItemText>
+        </StyledHeroItem>
+        <StyledHeroItem>
+          <img src={icon2} alt="icon" width="30" height="30" />
+          <StyledItemText>Швидка доставка</StyledItemText>
+        </StyledHeroItem>
+        <StyledHeroItem>
+          <img src={icon3} alt="icon" width="30" height="30" />
+          <StyledItemText>Оплата при отриманні</StyledItemText>
+        </StyledHeroItem>
+      </StyledHeroList>
     </>
   );
 };
--- src/components/Hero/Hero.styled.js
+++ src/components/Hero/Hero.styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { colors } from 'styles/globalStyles';
 
 export const StyledMainTitle = styled.h1`
   padding: 10px 20px;
@@ -10,4 +11,30 @@ export const StyledMainTitle = styled.h1`
 
 export const StyledHeroImg = styled.img`
   max-width: 100%;
+`;
+
+export const StyledHeroList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px 20px;
+  min-width: 320px;
+
+  background-color: ${colors.mainBgColor};
+`;
+
+export const StyledHeroItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  list-style: none;
+`;
+
+export const StyledItemText = styled.p`
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 1.1;
+
+  color: ${colors.mainTextColor};
 `;
